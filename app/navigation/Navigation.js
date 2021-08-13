93a8eecc4940c4a8dcb7bb68ee9ca9ffd64dfa9e
Nahuel Costamagna <nlcostamagna@gmail.com>
2021-08-13 09:14:54 -0300

    feat: style

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Text, StyleSheet } from "react-native"
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {Icon} from "react-native-elements"
@@ -8,6 +9,15 @@ import EventStack from './EventStack';
 import ProfileStack from './ProfileStack';
 import ConfigStack from './ConfigStack';
 
+const COLOR_BLUE = "#0052CF";
+const OPTION_SCREEN = {
+    headerTitleAlign: 'center',
+    headerStyle: {
+        backgroundColor: COLOR_BLUE,
+      },
+    headerTintColor: "#FFFFFF"
+};
+
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
@@ -15,43 +25,53 @@ export default function Navigation() {
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="candelar-stack"
-        tabBarOptions={{
-            inactiveTintColor: "#646464",
-            activeTintColor: "#00a680",
-        }}
+
         screenOptions={({route}) => ({
-            tabBarIcon: ({color}) => screenOptions(route, color),
-            tabBarLabel:() => {return null},
+            tabBarIcon: ({color, focused}) => screenIcon(route, focused, color),
+            tabBarLabel:({focused, color}) => screenText(route,color, focused),
+            inactiveTintColor: "#484848",
+            activeTintColor: COLOR_BLUE,
+            headerTitleStyle: { alignSelf: 'center' },
+            tabBarStyle:{
+                paddingTop: 5,
+                height: 55
+            }
+            
         })}
       >
         <Tab.Screen 
             name="contact-stack" 
             component={ContactStack} 
             options={{
-                title: "Contact",
+                ...OPTION_SCREEN,
+                title:"Contact"
             }}/>
         <Tab.Screen 
             name="event-stack" 
             component={EventStack} 
             options={{
+                ...OPTION_SCREEN,
                 title: "Event",
             }}/>
         <Tab.Screen 
             name="calendar-stack" 
             component={CalendarStack} 
             options={{
+                ...OPTION_SCREEN,
                 title: "Calendar",
             }}/>
         <Tab.Screen 
             name="profile-stack" 
             component={ProfileStack} 
             options={{
+                ...OPTION_SCREEN,
                 title: "Profile",
             }}/>
         <Tab.Screen 
             name="config-stack" 
             component={ConfigStack} 
             options={{
+                ...OPTION_SCREEN,
                 title: "Configuration",
             }}/>
       </Tab.Navigator>
@@ -59,7 +79,7 @@ export default function Navigation() {
   );
 }
 
-function screenOptions(router, color){
+function screenIcon(router, focused, color){
     let iconName;
 
     switch(router.name) {
@@ -86,3 +106,45 @@ function screenOptions(router, color){
         <Icon type="material-community" name={iconName} size={30} color={color} />
     )
 }
+
+
+function screenText(router, color, focused){
+    let label;
+
+    if (focused) {
+        switch(router.name) {
+            case "contact-stack":
+                label = "Contact";
+                break;
+            case "event-stack":
+                label = "Event";
+                break;
+            case "calendar-stack":
+                label = "Calendar";
+                break;
+            case "profile-stack":
+                label = "Profile";
+                break;
+            case "config-stack":
+                label = "Config";
+                break;
+            default:
+                break;
+        }
+
+        return (
+            <Text style={styles.activeColor}>{label}</Text>
+         )
+    }
+    
+    return null
+    
+}
+
+const styles = StyleSheet.create({
+    activeColor: {
+      color: COLOR_BLUE,
+      fontSize: 11,
+      paddingBottom: 4,
+    },
+});
