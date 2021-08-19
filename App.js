d9e75ae26e58d5d06ac10654b831c2a0c19157b1
Nahuel Costamagna <nlcostamagna@gmail.com>
2021-08-19 00:07:27 -0300

    feat: add native base

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import Navigation from './app/navigation/Navigation';
 import Login from './app/page/Login/Login'
+import { NativeBaseProvider, Text, Box } from 'native-base';
 
 export default function App() {
 
   [user, setUser] = useState(null)
 
-  return user ? <Navigation 
+  let app = user ? <Navigation 
                   user={user}
                   setUser={setUser}
                   /> : 
@@ -14,4 +15,10 @@ export default function App() {
                   user={user}
                   setUser={setUser}
                   />
+
+    return (
+      <NativeBaseProvider>
+        {app}
+      </NativeBaseProvider>
+    )
 }
--- app/page/Login/Login.js
+++ app/page/Login/Login.js
@@ -1,35 +1,38 @@
-import React from "react";
-import { View, StyleSheet } from "react-native"
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
+import React, {useState} from "react";
+import { StyleSheet} from "react-native"
+import { Button, Input, HStack, Stack, Center, NativeBaseProvider  } from 'native-base';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Login(props) {
   
     const {user, setUser} = props
-
-    const onSubmit = () => {
-        setUser({name: "Nahuel"})
+    const [show, setShow] = useState(false)
+    const handleClick = () => setShow(!show)
+    const onSubmit = () => { 
+      setUser({name: "Nahuel"})
     }   
   return (
-      <View  style={styles.formContainer}>    
-      <Input    
-        title="User"
-      />
-      <Input
-        title="Password"
-      />
+    <Center flex={1}>
+     <Stack space={2}  w="90%" >
+        <Stack space={2} direction={"column"}>
+            <Input placeholder="Usuario" mt={5} />
+            <Input placeholder="Contraseña"  type={show ? "text" : "password"}
+              InputRightElement={
+                <Button ml={1} roundedLeft={0} roundedRight="md" onPress={handleClick}>
+                  <Ionicons name={show?"eye-off":"eye"} size={20} color="white" />
 
-      <Button
-        title="Login"
-        isRow={true}
-        onPress={onSubmit}
-      />
-      <Button
-        isRow={true}
-        title="Create Account"
-      />
-
-      </View>
+                </Button>
+              }
+            />
+        </Stack>
+        </Stack>
+        <Stack space={2} alignItems="center" mt={10}>
+        <Stack space={2} direction={"row"}>
+            <Button onPress={onSubmit} paddingLeft={10} paddingRight={10} >Login</Button>
+            <Button>Crear cuenta</Button>
+        </Stack>
+      </Stack>
+    </Center>
   )
 }
 
